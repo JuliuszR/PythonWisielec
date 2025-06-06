692d5eb926c2691af7aeae2ee85ce969d3c02081
Paulina Mrzyglod <paulina.mrzyglod.0014@gmail.com>
2025-06-06 16:02:17 +0200

    Menu dodanie przyciskow

--- Projekt/GUI/menu.py
+++ Projekt/GUI/menu.py
@@ -25,15 +25,35 @@ class MainMenuWindow:
 
         image_path = os.path.join(parent_dir, "Assets", "nowaGra.png")
         button_img = Image.open(image_path)
-        button_img = button_img.resize((200, 80), Image.Resampling.LANCZOS)
+        button_img = button_img.resize((210, 100), Image.Resampling.LANCZOS)
         self.new_game_image = ImageTk.PhotoImage(button_img)
 
-        self.button_id = self.canvas.create_image(400, 300, image=self.new_game_image)
+        image_path = os.path.join(parent_dir, "Assets", "logowanie.png")
+        button_img = Image.open(image_path)
+        button_img = button_img.resize((210, 100), Image.Resampling.LANCZOS)
+        self.logowanie_image = ImageTk.PhotoImage(button_img)
+
+        image_path = os.path.join(parent_dir, "Assets", "wyjscie.png")
+        button_img = Image.open(image_path)
+        button_img = button_img.resize((210, 100), Image.Resampling.LANCZOS)
+        self.wyjscie_image = ImageTk.PhotoImage(button_img)
+
+        self.button_id = self.canvas.create_image(400, 260, image=self.new_game_image)
+        self.button_logowanie = self.canvas.create_image(400, 320, image=self.logowanie_image)
+        self.button_wyjscie = self.canvas.create_image(400, 380, image=self.wyjscie_image)
 
         self.canvas.tag_bind(self.button_id, "<Button-1>", self.start_new_game)
+        self.canvas.tag_bind(self.button_logowanie, "<Button-1>", self.logowanie)
+        self.canvas.tag_bind(self.button_wyjscie, "<Button-1>", self.wyjscie)
 
     def start_new_game(self, event):
         print("Nowa gra kliknięta!")
+
+    def wyjscie(self, event):
+        print("Wyjscie")
+
+    def logowanie(self, event):
+        print("Logowanie")
 
 if __name__ == "__main__":
     root = tk.Tk()
